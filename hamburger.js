42e7677befa21645fed9ef85a28725823d4c0f42
emma <emmanuelsunday555555@gmail.com>
2026-06-26 08:49:29 -0700

    uodate

---
+++ hamburger.js
@@ -0,0 +1,7 @@
+const burger = document.getElementById("emma");
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", () =>{
+    emma.classList.toggle("show");
+
+})
